47ec81970fb958f8d8c47f060080146186973704
Tengku1 <anountengku@gmail.com>
2023-06-17 21:33:50 +0800

    create register method

--- lib/pages/auth/auth_controller.dart
+++ lib/pages/auth/auth_controller.dart
@@ -6,7 +6,8 @@ import 'package:mobile_ta/widgets/widget_error_screen.dart';
 import 'package:http/http.dart' as http;
 
 class AuthController extends GetxController {
-  final usernameController = TextEditingController();
+  final nameController = TextEditingController();
+  final surnameController = TextEditingController();
   final pwdController = TextEditingController();
   final emailController = TextEditingController();
   final phoneController = TextEditingController();
@@ -15,7 +16,10 @@ class AuthController extends GetxController {
   final usersData = {}.obs;
 
   void resetForm() {
-    usernameController.text = "";
+    nameController.text = "";
+    surnameController.text = "";
+    phoneController.text = "";
+    emailController.text = "";
     pwdController.text = "";
   }
 
@@ -37,6 +41,35 @@ class AuthController extends GetxController {
       final responseData = jsonDecode(response.body);
       usersData.value = responseData;
       return Get.toNamed("/");
+    } catch (e) {
+      return Get.to(() => ErrorScreen(
+            headMessage: "Error",
+            message: "{$e.message}",
+          ));
+    }
+  }
+
+  Future<void> register() async {
+    final url = Uri.parse('$ip/hotels/register');
+    final data = {
+      'email': emailController.text,
+      'password': pwdController.text,
+      'name': nameController.text,
+      'surname': surnameController.text,
+      'telephone': phoneController.text
+    };
+
+    try {
+      if (usersData.isNotEmpty) {
+        return Get.offAllNamed("/");
+      }
+      final response = await http.post(url,
+          body: jsonEncode(data),
+          headers: {'Content-Type': 'application/json'});
+
+      final responseData = jsonDecode(response.body);
+      usersData.value = responseData;
+      Get.offAndToNamed("/");
     } catch (e) {
       return Get.to(() => ErrorScreen(
             headMessage: "Error",
