92a2588fa8a7410495d525ae99ab60609495782e
Tengku1 <anountengku@gmail.com>
2023-06-30 15:37:21 +0800

    reroll main_controller at hotel payment method

--- lib/pages/Hotel_Payment_Method_Page/main_controller.dart
+++ lib/pages/Hotel_Payment_Method_Page/main_controller.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
 import 'package:mobile_ta/configs/api_config.dart';
 import 'package:mobile_ta/pages/Hotel_Payment_Method_Page/main_page.dart';
 import 'package:mobile_ta/widgets/widget_error_screen.dart';
@@ -6,6 +9,7 @@ import 'package:mobile_ta/widgets/widget_error_screen.dart';
 class PaymentController extends GetxController {
   var selectedPaymentMethod = 'Transfer Bank Mandiri'.obs;
   var selectedPaymentType = 'BCA'.obs;
+  final ip = "http://192.168.18.7:3000";
 
   void setSelectedPaymentMethod(
       String paymentMethod, String paymentMethodType) {
@@ -14,10 +18,15 @@ class PaymentController extends GetxController {
   }
 
   Future<void> confirmBook(List ids) async {
+    final url = Uri.parse('$ip/hotels/bookings');
     final data = {"id": '${ids[1]}', "bookId": '${ids[0]}'};
 
     try {
-      final response = await postReq("bookings", data);
+      final response = await http.post(
+        url,
+        body: jsonEncode(data),
+        headers: {'Content-Type': 'application/json'},
+      );
       if (response.statusCode == 201) {
         Get.to(() => const PaymentCompletedPage());
       }
